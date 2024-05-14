9286cd1868fb58e1161b248a1838c99619fe374a
Kilemonn <kylegonzalez2255@gmail.com>
2024-05-14 23:34:10 +0900

    Add place holder if an object is empty. Add recursive resolution if the object property is another object.

--- model-visualiser/main.py
+++ model-visualiser/main.py
@@ -1,20 +1,20 @@
 import json
-from typing import Optional
 
 import graphviz
 
 
 def main():
+    output_format = "png"
     graph = graphviz.Digraph(node_attr={'shape': 'record'})
     file_name = "test.json"
     with open(file_name, 'r') as file:
         model = json.loads(file.read())
         create_nodes(graph, model)
 
-    graph.render(outfile='diagram.png', format="png")
+    graph.render(outfile=file_name.split(".")[0] + "." + output_format, format=output_format)
 
 
-def create_nodes(graph: graphviz.Digraph, model: dict, name: str = "root") -> Optional[str]:
+def create_nodes(graph: graphviz.Digraph, model: dict, name: str = "root") -> str:
     ret = None
     with graph.subgraph(name="cluster_" + name) as g:
         g.attr(label=name)
@@ -22,18 +22,21 @@ def create_nodes(graph: graphviz.Digraph, model: dict, name: str = "root") -> Op
             key_type = type(model[key])
             property_name = key + " - " + key_type.__name__
             if isinstance(model[key], list):
-                l = model[key]
+                l: list = model[key]
                 if len(l) > 0:
                     list_type = type(l[0])
-                    # if isinstance(l[0], dict):
-                    #     # Check array type, if it is a complex type, recurse
-                    #     pass
-                    # else:
+                    if isinstance(l[0], dict):
+                        r = create_nodes(graph, l[0], key)
+                        if r is None:
+                            # empty?
+                            pass
+                        else:
+                            g.edge(key, r)
                     property_name = key + " - " + key_type.__name__ + "[" + list_type.__name__ + "]"
 
             if ret is None:
                 ret = property_name
-            g.node(name=property_name, shape="record")
+            g.node(name=property_name, label=property_name, shape="record")
             if isinstance(model[key], dict):
                 r = create_nodes(graph, model[key], key)
                 if r is None:
@@ -42,6 +45,9 @@ def create_nodes(graph: graphviz.Digraph, model: dict, name: str = "root") -> Op
                 else:
                     g.edge(key, r)
 
+        if ret is None:
+            ret = name + "_empty"
+            g.node(name=ret, label="")
     return ret
 
 
