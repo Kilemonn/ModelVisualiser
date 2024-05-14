0a58c36ce849b23fbc6bb210984c0b46f3c4d46c
Kilemonn <kylegonzalez2255@gmail.com>
2024-05-14 23:46:39 +0900

    Move list and dict handlers to their own methods.

--- model-visualiser/main.py
+++ model-visualiser/main.py
@@ -22,33 +22,42 @@ def create_nodes(graph: graphviz.Digraph, model: dict, name: str = "root") -> st
             key_type = type(model[key])
             property_name = key + " - " + key_type.__name__
             if isinstance(model[key], list):
-                l: list = model[key]
-                if len(l) > 0:
-                    list_type = type(l[0])
-                    if isinstance(l[0], dict):
-                        r = create_nodes(graph, l[0], key)
-                        if r is None:
-                            # empty?
-                            pass
-                        else:
-                            g.edge(key, r)
-                    property_name = key + " - " + key_type.__name__ + "[" + list_type.__name__ + "]"
+                property_name = handle_list(g, graph, model[key], key, property_name)
 
             if ret is None:
                 ret = property_name
             g.node(name=property_name, label=property_name, shape="record")
             if isinstance(model[key], dict):
-                r = create_nodes(graph, model[key], key)
-                if r is None:
-                    # empty object?
-                    pass
-                else:
-                    g.edge(key, r)
+                handle_dict(g, graph, model[key], key)
 
         if ret is None:
             ret = name + "_empty"
             g.node(name=ret, label="")
     return ret
+
+
+def handle_list(subgraph: graphviz.Digraph, graph: graphviz.Digraph, model: list, key: str, property_name: str) -> str:
+    if len(model) > 0:
+        list_type = type(model[0])
+        if isinstance(model[0], dict):
+            r = create_nodes(graph, model[0], key)
+            if r is None:
+                # empty?
+                pass
+            else:
+                subgraph.edge(key, r)
+        else:
+            property_name = property_name + "[" + list_type.__name__ + "]"
+    return property_name
+
+
+def handle_dict(subgraph: graphviz.Digraph, graph: graphviz.Digraph, model: dict, key: str):
+    r = create_nodes(graph, model, key)
+    if r is None:
+        # empty object?
+        pass
+    else:
+        subgraph.edge(key, r)
 
 
 if __name__ == '__main__':
