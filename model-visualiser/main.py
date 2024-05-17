d5d463398ea7ac3fe756889b6eabfe4087a6982e
Kilemonn <kylegonzalez2255@gmail.com>
2024-05-17 19:33:10 +0900

    Add further test json files and update main to take path as an arg.

--- model-visualiser/main.py
+++ model-visualiser/main.py
@@ -1,34 +1,46 @@
 import json
+import sys
 
 import graphviz
+
+ROOT_PATH: str = "root"
+PERIOD = "."
+EMPTY_SUFFIX: str = "_empty"
+CLUSTER_PREFIX: str = "cluster_"
 
 
 def main():
     output_format = "png"
-    file_name = "test.json"
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+    else:
+        print("Expected JSON file path as first program argument.")
+        return
 
-    graph = graphviz.Digraph(node_attr={'shape': 'record'})
-    with open(file_name, 'r') as file:
+    graph = graphviz.Digraph()
+    with open(file_name, "r") as file:
         model = json.loads(file.read())
         create_nodes(graph, model)
     graph.render(outfile=output_file_name(file_name, output_format), format=output_format)
 
 
-def create_nodes(graph: graphviz.Digraph, model: dict, name_path: str = "root") -> str:
-    placeholder_name = name_path + "_empty"
-    with graph.subgraph(name="cluster_" + name_path) as g:
+def create_nodes(graph: graphviz.Digraph, model: dict, name_path: str = ROOT_PATH) -> str:
+    placeholder_name = name_path + EMPTY_SUFFIX
+    with graph.subgraph(name=CLUSTER_PREFIX + name_path) as g:
         g.attr(label=name_path)
         for key in model:
             key_type = type(model[key])
-            property_name = name_path + "/" + key + " - " + key_type.__name__
+            property_path = name_path + "/" + key
+            property_name = property_path + " - " + key_type.__name__
             if isinstance(model[key], list):
-                property_name = handle_list(g, graph, model[key], name_path + "/" + key, property_name)
-
+                property_name = handle_list(g, graph, model[key], property_path, property_name)
             if isinstance(model[key], dict):
-                handle_dict(g, graph, model[key], name_path + "/" + key)
+                handle_dict(g, graph, model[key], property_path)
             else:
-                g.node(name=property_name, label=property_name, shape="record")
+                g.node(name=property_name, label=property_name)
+
         g.node(name=placeholder_name, label="")
+
     return placeholder_name
 
 
@@ -37,11 +49,7 @@ def handle_list(subgraph: graphviz.Digraph, graph: graphviz.Digraph, model: list
         list_type = type(model[0])
         if isinstance(model[0], dict):
             r = create_nodes(graph, model[0], key)
-            if r is None:
-                # empty?
-                pass
-            else:
-                subgraph.edge(key, r)
+            subgraph.edge(key, r)
         else:
             property_name = property_name + "[" + list_type.__name__ + "]"
     return property_name
@@ -49,15 +57,10 @@ def handle_list(subgraph: graphviz.Digraph, graph: graphviz.Digraph, model: list
 
 def handle_dict(subgraph: graphviz.Digraph, graph: graphviz.Digraph, model: dict, key: str):
     r = create_nodes(graph, model, key)
-    if r is None:
-        # empty object?
-        pass
-    else:
-        subgraph.edge(key, r)
+    subgraph.edge(key, r)
 
 
 def output_file_name(input_file_name: str, output_format: str) -> str:
-    PERIOD = "."
     split = input_file_name.split(PERIOD)
     if len(split) > 0:
         return split[0] + PERIOD + output_format
